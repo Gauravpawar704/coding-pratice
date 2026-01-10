31c5254514e999c05be8d60631a7e5d56c582746
Gauravpawar704 <gauravpawar9845@gmail.com>
2026-01-10 11:32:04 +0530

    Add files via upload

---
+++ binu.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[10], n, key;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter sorted elememnts:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int low = 0, high = n - 1, mid;
+    bool found = false;
+
+    while (low <= high)
+    {
+        mid=(low + high) / 2;
+        if (arr[mid] == key)
+        {
+            cout << "Elemet found at position" << mid + 1;
+            found = true;
+            break;
+        }
+        else if (arr[mid] < key)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    if (!found)
+    {
+        cout << "Element not found";
+    }
+}
