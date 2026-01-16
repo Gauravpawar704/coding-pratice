325325a1e96b2d7962b70beffbb8cc96146e46bc
Gauravpawar704 <gauravpawar9845@gmail.com>
2026-01-16 18:01:56 +0530

    Add files via upload

---
+++ Function/power_recursion.cpp
@@ -0,0 +1,20 @@
+// Program:Power using recursion
+// author: Gaurav
+#include <iostream>
+using namespace std;
+int power(int base, int exp)
+{
+    if (exp == 0)
+    {
+        return 1;
+    }
+    return base * power(base, exp - 1);
+}
+int main()
+{
+    int b, e;
+    cout << "Enter base and exponent: ";
+    cin >> b >> e;
+
+    cout << "Result: " << power(b, e);
+}
