c5f51dd37622c8b7b9af387c25c99d7d920f0794
Gauravpawar704 <gauravpawar9845@gmail.com>
2026-01-16 18:51:49 +0530

    Add reverse string program in C++
    The program takes a string input from the user and prints it in reverse order.

---
+++ Pattern_string/reverse_string.cpp
@@ -0,0 +1,15 @@
+// Program:Reverse a string
+// author: Gaurav
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+    for (int i = s.length() - 1; i > 0; i--)
+    {
+        cout << s[i];
+    }
+    return 0;
+}
